a93bc0eac937fccd07d6b0f325b4816c64f73561
Obed Noe Martinez Gonzalez <68682799+SupaStar@users.noreply.github.com>
2026-01-15 14:30:51 -0600

    Funcional web

--- FinanzasDashFront/composeApp/src/webMain/kotlin/com/frontend/finanzasdashfront/utils/Extension.web.kt
+++ FinanzasDashFront/composeApp/src/webMain/kotlin/com/frontend/finanzasdashfront/utils/Extension.web.kt
@@ -2,8 +2,9 @@ package com.frontend.finanzasdashfront.utils
 
 import kotlin.js.js
 
+
 // webMain
 actual fun Float.formatCurrency(): String =
-    js(
-        "Number(this).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })"
-    ) as String
+    formatCurrencyJs(this)
+
+external fun formatCurrencyJs(value: Float): String
